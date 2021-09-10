fefc647175983d8a1f51bb2de5d5966395ed96d2
Lonong <1851153231@qq.com>
2021-09-10 16:40:06 +0800

    爬虫

---
+++ python/numpy/numpy01.py
@@ -0,0 +1,23 @@
+# encoding=utf-8
+import numpy
+from scipy import stats
+
+s = [20, 25, 30, 35, 35]
+
+x = numpy.mean(s)  # 平均数‘
+y = numpy.median(s)  # 中位数
+z = stats.mode(s)  # 众数
+c = numpy.std(s)  # 标准差
+v = numpy.var(s)  # 方差
+b=numpy.random.uniform(5,10,100)
+
+print(x)
+print(y)
+print(z)
+print(c)
+print(v)
+print(b)
+
+print(type(v))
+print(type(1))
+print(5.830951894845301 * 5.830951894845301)
